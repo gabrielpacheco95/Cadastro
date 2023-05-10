a6583da3852b44752a197aef61c52c3938cf181d
Gabriel Almeida Pacheco <gabrielpacheco42@gmail.com>
2023-05-10 09:32:58 -0300

    cadastro

---
+++ src/model/Pessoas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 311101245
+ */
+public class Pessoas {
+
+    private int idPessoa;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+}
